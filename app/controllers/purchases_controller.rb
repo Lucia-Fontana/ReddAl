f06fc189dff656f63edaf2ae1e596885af74abca
Lucia Fontana <lucia.fontana1987@gmail.com>
2023-05-10 14:15:48 +0200

    Payment new

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -16,12 +16,84 @@ class PurchasesController < ApplicationController
     @order = Order.find(params[:order_id])
   end
 
+  # def create
+  #   @user = current_user
+  #   @product = Product.find(params[:product_id])
+  #   order = Order.create!(total_price: @product.price, state: 'pending', user: current_user)
+  #   @purchase = Purchase.create!(product: @product, order: order)
+  #   redirect_to new_order_payment_path(@purchase.order)
+  # end
+
   def create
     @user = current_user
-    @product = Product.find(params[:product_id])
-    order = Order.create!(total_price: @product.price, state: 'pending', user: current_user)
-    @purchase = Purchase.create!(product: @product, order: order)
-    redirect_to new_order_payment_path(@purchase.order)
+    # for every purchase, check if the user has a pending order
+    if current_user&.orders&.last&.state == "pending"
+      # create a purchase, new session Stripe, update order with a new session
+
+      @purchase = Purchase.new
+      @purchase.order = current_user.orders.where(state: "pending").last
+      @purchase.product = Product.find(params[:product_id])
+      @purchase.save!
+      @product = Product.find(params[:product_id])
+      @product.availability = false
+      @product.save!
+      line_items = []
+      current_user.orders.where(state: "pending").last.products.each do |product|
+        line_items << {
+          price_data: {
+            currency: 'eur',
+            product_data: {
+              name: product.description,
+            },
+            unit_amount: product.price,
+          },
+          quantity: 1,
+        }
+      end
+
+      total = 0
+      @purchase.order.purchases.each do |purchase|
+        total += purchase.product.price
+      end
+
+      # session = Stripe::Checkout::Session.create(
+      #   payment_method_types: ['card'],
+      #   line_items: [{
+      #     price_data: {
+      #       currency: 'eur',
+      #       product_data: {
+      #         name: @purchase.product.description,
+      #       },
+      #       unit_amount: total,
+      #     },
+      #     quantity: 1,
+      #   }],
+      #   mode: 'payment',
+      #   success_url: order_url(@purchase.order),
+      #   cancel_url: order_url(@purchase.order)
+      # )
+
+      # @purchase.order.update(checkout_session_id: session.id, amount_cents: total)
+      # redirect_to new_order_payment_path(@purchase.order)
+      respond_to do |format|
+        format.html { redirect_to new_order_payment_path(@purchase.order) }
+        format.json { head :no_content }
+      end
+      flash[:notice] = "Product succesfully added"
+
+     else
+      # create an order, create the associated purchase and session stripe for the order
+      # change order with checkout_id
+      @product = Product.find(params[:product_id])
+      order  = Order.create!(total_price: @product.price, state: 'pending', user: current_user)
+      @purchase = Purchase.create!(product: @product, order: order)
+
+      # redirect_to new_order_payment_path(@purchase.order)
+      respond_to do |format|
+        format.html { redirect_to new_order_payment_path(@purchase.order) }
+        format.json { head :no_content }
+      end
+    end
   end
 
   def destroy
